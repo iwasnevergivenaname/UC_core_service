13cf8074c2bf252311a0d11af681be2e22bb55b4
iwasnevergivenaname <hannah.mace93@gmail.com>
2021-08-16 12:04:54 -0700

    get trips by id, riderid, and driverid datasource created

--- src/datasources/routeSuggestion.js
+++ src/datasources/routeSuggestion.js
@@ -6,7 +6,32 @@ class RouteSuggestionAPI extends RESTDataSource {
     this.baseURL = "http://localhost:3002/routing"
   }
 
+  async getTripById({tripId}) {
+    const res = await this.get('trip', {id: tripId})
+    return res[0]
+  }
+
+  async getAllRidersTrips({riderId}) {
+    const res = await this.get('ridersTrips', {riderId: riderId})
+    return Array.isArray(res) ? res.map(t => this.tripReducer(t)) : []
+  }
+
+  async getAllDriversTrips({riderId}) {
+    const res = await this.get('driversTrips', {driverId: riderId})
+    return Array.isArray(res) ? res.map(t => this.tripReducer(t)) : []
+  }
+
 //  get the route suggestion
+
+  tripReducer(trip) {
+    return {
+      id: trip.id,
+      rider: trip.riderId,
+      driver: trip.driverId,
+      pickup: trip.pickup,
+      dropoff: trip.dropoff
+    }
+  }
 }
 
 module.exports = RouteSuggestionAPI
